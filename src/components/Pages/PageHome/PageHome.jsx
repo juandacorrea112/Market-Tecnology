4c7f9864a72ddce73b75c33aa6791e5800e6a1b3
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2022-12-16 22:37:22 -0500

    Nuevo apartado en proceso

--- src/components/Pages/PageHome/PageHome.jsx
+++ src/components/Pages/PageHome/PageHome.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { PagehomeCen } from '../../Ui/PagehomeCen/PagehomeCen'
 import { PagehomeUp } from '../../Ui/PagehomeUp/PagehomeUp'
 
 export const PageHome = () => {
   return (
     <div>
         <PagehomeUp/>
+        <PagehomeCen/>
     </div>
   )
 }
---
+++ src/components/Ui/PagehomeCen/PagehomeCen.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './PagehomeCen.css'
+
+export const PagehomeCen = () => {
+  return (
+    <div className='PatherBackgImg'>
+        <div className="lentDarkandCont">
+            <div className="contTittleDestac">
+                <h1 className='TittlePrinciple'>PRODUCTOS MAS DESTACADOS</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
