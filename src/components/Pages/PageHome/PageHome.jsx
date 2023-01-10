b0bb1f9ecc693723cf88f78a633de476909e4f3a
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2023-01-10 13:09:27 -0500

    nueva carta de productos en linea establecida y nuevo page

--- src/components/Pages/PageHome/PageHome.jsx
+++ src/components/Pages/PageHome/PageHome.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { PagehomeCen } from '../../Ui/PagehomeCen/PagehomeCen'
 import { PagehomeUp } from '../../Ui/PagehomeUp/PagehomeUp'
+import { Pagehomebottom } from '../../Ui/PagehomeBottom/Pagehomebottom'
 
 export const PageHome = () => {
   return (
     <div>
         <PagehomeUp/>
         <PagehomeCen/>
+        <Pagehomebottom/>
     </div>
   )
 }
--- src/components/Ui/PagehomeBottom/Pagehomebottom.jsx
+++ src/components/Ui/PagehomeBottom/Pagehomebottom.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import './Pagehomebottom.css'
+import  logopc2do  from '../../../images/logopc2do.jpg'
 
 export const Pagehomebottom = () => {
   return (
-    <div>
-        
+    <div className='contServicios'>
+        <div className="contTittleS">
+          <h1>PRODUCTOS EN VENTA</h1>
+        </div>
+        <div className="contCardsS">
+          <div className="card">
+            <div className="face front">
+              <img src={logopc2do} alt="..." />
+              <h3>venta de pc</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Rem commodi beatae quidem reprehenderit nostrum 
+                doloremque officiis sapiente
+                nam cumque dolorum, in nesciunt totam consectetur at quia 
+                iure quos!</p>
+                <div className="link">
+                  <a href="#">Details</a>
+                </div>
+            </div>
+            <div className="face back">
+            <h3>venta de pc</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Rem commodi beatae quidem reprehenderit nostrum 
+                doloremque officiis sapiente
+                nam cumque dolorum, in nesciunt totam consectetur at quia 
+                iure quos!</p>
+              <div className="link">
+                <a href="#">Details</a>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
