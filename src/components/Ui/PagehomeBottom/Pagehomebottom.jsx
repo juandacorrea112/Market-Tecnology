6fe9d807adc44984270d800512c84101d0833b8f
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2023-01-10 20:34:13 -0500

    apartado de productos en venta terminado temporalmente

--- src/components/Ui/PagehomeBottom/Pagehomebottom.jsx
+++ src/components/Ui/PagehomeBottom/Pagehomebottom.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Pagehomebottom.css'
 import  logopc2do  from '../../../images/logopc2do.jpg'
+import cellProducto from '../../../images/productos/cellProducto.jpg'
+import pcProductoVenta from '../../../images/productos/pcProductoVenta.jpg'
 
 export const Pagehomebottom = () => {
   return (
@@ -11,24 +13,54 @@ export const Pagehomebottom = () => {
         <div className="contCardsS">
           <div className="card">
             <div className="face front">
-              <img src={logopc2do} alt="..." />
-              <h3>venta de pc</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Rem commodi beatae quidem reprehenderit nostrum 
-                doloremque officiis sapiente
-                nam cumque dolorum, in nesciunt totam consectetur at quia 
-                iure quos!</p>
-                <div className="link">
-                  <a href="#">Details</a>
-                </div>
+              <img src={cellProducto} alt="..." />
+              <h3>Venta De Celular</h3>
             </div>
             <div className="face back">
-            <h3>venta de pc</h3>
+              <h3>venta de pc</h3>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Rem commodi beatae quidem reprehenderit nostrum 
-                doloremque officiis sapiente
-                nam cumque dolorum, in nesciunt totam consectetur at quia 
-                iure quos!</p>
+              Rem commodi beatae quidem reprehenderit nostrum 
+              doloremque officiis sapiente
+              nam cumque dolorum, in nesciunt totam consectetur at quia 
+              iure quos!</p>
+              <div className="link">
+                <a href="#">Details</a>
+              </div>
+            </div>
+          </div>
+
+
+          <div className="card">
+            <div className="face front">
+              <img src={logopc2do} alt="..." />
+              <h3>Venta De Pc</h3>
+            </div>
+            <div className="face back">
+              <h3>venta de pc</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Rem commodi beatae quidem reprehenderit nostrum 
+              doloremque officiis sapiente
+              nam cumque dolorum, in nesciunt totam consectetur at quia 
+              iure quos!</p>
+              <div className="link">
+                <a href="#">Details</a>
+              </div>
+            </div>
+          </div>
+
+
+          <div className="card">
+            <div className="face front">
+              <img src={pcProductoVenta} alt="..." />
+              <h3>Venta De Pc</h3>
+            </div>
+            <div className="face back">
+              <h3>venta de pc</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Rem commodi beatae quidem reprehenderit nostrum 
+              doloremque officiis sapiente
+              nam cumque dolorum, in nesciunt totam consectetur at quia 
+              iure quos!</p>
               <div className="link">
                 <a href="#">Details</a>
               </div>
