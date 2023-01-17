b1febe3245c6a0b6b1230629e72fa828527fc6a2
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2023-01-16 21:51:23 -0500

    sideBar incorporado en el proyecto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { PageHome } from './components/Pages/PageHome/PageHome';
 import { Footer } from './components/Layouts/Footer/Footer'
+import { Navbar } from './components/Layouts/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <PageHome/>
       <Footer/>
     </div>
--- src/components/Layouts/Navbar/Navbar.jsx
+++ src/components/Layouts/Navbar/Navbar.jsx
@@ -1,10 +1,147 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 
 export const Navbar = () => {
+
+  const [sidebarNav, setSidebarnav] = useState("close")
+
+  const OpenMenu = () => {
+    setSidebarnav("showMenu")
+  }
+  const CloseMenu = () => {
+    setSidebarnav("close")
+  }
+
   return (
-    <div>
-        
+    <>
+      <div className={`sidebar ${sidebarNav}`}>
+    <div class="logo-details">
+      {sidebarNav == "close" &&
+        <i onClick={OpenMenu} className='bx bx-menu' style={{cursor:"pointer"}} ></i>
+      }
+
+      {sidebarNav == "showMenu" &&
+        <i onClick={CloseMenu} className='bx bx-menu' style={{cursor:"pointer"}} ></i>
+      }
+
+      <span className="logo_name">Market Tecnology</span>
     </div>
+
+    <ul class="nav-links">
+      <li>
+        <a href="#">
+          <i class='bx bx-grid-alt' ></i>
+          <span class="link_name">Dashboard</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">Category</a></li>
+        </ul>
+      </li>
+      <li>
+        <div class="iocn-link">
+          <a href="#">
+            <i class='bx bx-collection' ></i>
+            <span class="link_name">Category</span>
+          </a>
+          <i class='bx bxs-chevron-down arrow' ></i>
+        </div>
+        <ul class="sub-menu">
+          <li><a class="link_name" href="#">Category</a></li>
+          <li><a href="#">HTML & CSS</a></li>
+          <li><a href="#">JavaScript</a></li>
+          <li><a href="#">PHP & MySQL</a></li>
+        </ul>
+      </li>
+      <li>
+        <div class="iocn-link">
+          <a href="#">
+            <i class='bx bx-book-alt' ></i>
+            <span class="link_name">Posts</span>
+          </a>
+          <i class='bx bxs-chevron-down arrow' ></i>
+        </div>
+        <ul class="sub-menu">
+          <li><a class="link_name" href="#">Posts</a></li>
+          <li><a href="#">Web Design</a></li>
+          <li><a href="#">Login Form</a></li>
+          <li><a href="#">Card Design</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#">
+          <i class='bx bx-pie-chart-alt-2' ></i>
+          <span class="link_name">Analytics</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">Analytics</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#">
+          <i class='bx bx-line-chart' ></i>
+          <span class="link_name">Chart</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">Chart</a></li>
+        </ul>
+      </li>
+      <li>
+        <div class="iocn-link">
+          <a href="#">
+            <i class='bx bx-plug' ></i>
+            <span class="link_name">Plugins</span>
+          </a>
+          <i class='bx bxs-chevron-down arrow' ></i>
+        </div>
+        <ul class="sub-menu">
+          <li><a class="link_name" href="#">Plugins</a></li>
+          <li><a href="#">UI Face</a></li>
+          <li><a href="#">Pigments</a></li>
+          <li><a href="#">Box Icons</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#">
+          <i class='bx bx-compass' ></i>
+          <span class="link_name">Explore</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">Explore</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#">
+          <i class='bx bx-history'></i>
+          <span class="link_name">History</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">History</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#">
+          <i class='bx bx-cog' ></i>
+          <span class="link_name">Setting</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">Setting</a></li>
+        </ul>
+      </li>
+      <li>
+    <div class="profile-details">
+      <div class="profile-content">
+        {/* <img src="image/profile.jpg" alt="profileImg"> */}
+      </div>
+      <div class="name-job">
+        <div class="profile_name">Prem Shahi</div>
+        <div class="job">Web Desginer</div>
+      </div>
+      <i class='bx bx-log-out' ></i>
+    </div>
+  </li>
+</ul>
+  </div>
+
+    </>
   )
 }
