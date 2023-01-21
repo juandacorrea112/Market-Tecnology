96f211eee6dbe9d79ee57f7f543dd71eb6ccb5c5
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2023-01-20 20:54:03 -0500

    archivos de registro creados

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { PageHome } from './components/Pages/PageHome/PageHome';
 import { Footer } from './components/Layouts/Footer/Footer'
 import { Navbar } from './components/Layouts/Navbar/Navbar';
 import { Route, Routes } from 'react-router-dom';
-import { PageLogin } from './components/Pages/PageLogin/PageLogin';
 
+//===== VISTAS =====//
+import { PageLogin } from './components/Pages/PageLogin/PageLogin';
+import { PageRegister } from './components/Pages/PageRegister/PageRegister';
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
       <Routes>
         <Route path='/' element={<PageHome/>}/>
         <Route path='/Login' element={<PageLogin/>}/>
+        <Route path='/Registro' element={<PageRegister/>}/>
       </Routes>
       {/* =============================== */}
       <Footer/>
---
+++ src/components/Pages/PageRegister/PageRegister.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './PageRegister.css'
+
+export const PageRegister = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
