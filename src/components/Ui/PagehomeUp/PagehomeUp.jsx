42a87fcee769a30841a803ed61e36e9cea7d2f25
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2023-02-13 00:06:36 -0500

    video del home establecido

--- src/components/Ui/PagehomeUp/PagehomeUp.jsx
+++ src/components/Ui/PagehomeUp/PagehomeUp.jsx
@@ -1,19 +1,61 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import FondoTecnológicoMundoDigital from '../../../images/video/FondoTecnológicoMundoDigital.mp4'
+import ComputadoraVideo from '../../../images/video/ComputadoraVideo.mp4'
 import './PagehomeUp.css'
 
 export const PagehomeUp = () => {
   return (
     <>
-    {/* <video playsInline autoPlay loop muted src={FondoTecnológicoMundoDigital} className='backgroundVideo' ></video> */}
-    <div className='ContPrinHomeUp'>
-      <div className="contTittleP">
-        <h2 className='TextAcompa'>Todo sobre la tecnologia</h2>
-        <h1 className='TittlePrinciple'>Market Tecnology</h1>
-        <h2 className='TextAcompa'>encuentra tu PC Celular o Consola ideal</h2>
-      </div>
-      <div className="contButtonsRs">
+      <main id='hero'>
+        <div className='ContPrinHomeUp'>
+          <div className="contTittleP">
+            <h2 className='TextAcompa'>Todo sobre la tecnologia</h2>
+            <h1 className='TittlePrinciple'>Market Tecnology</h1>
+            <h2 className='TextAcompa'>encuentra tu PC Celular o Consola ideal</h2>
+          </div>
+          <div className="contButtonsRs">
+            
+            <Link to='/Login'>
+              <button className="BtnP SecondClasBtn">
+                <span className="circleMovil" aria-hidden="true">
+                <span className="icon arrow"></span>
+                </span>
+                <span className="buttotext">INICIAR SESION</span>
+              </button>
+            </Link>
+              
+            <Link to='/Registro'>
+              <button className="BtnP SecondClasBtn">
+                <span className="circleMovil" aria-hidden="true">
+                <span className="icon arrow"></span>
+                </span>
+                <span className="buttotext">REGISTRARSE</span>
+              </button>
+            </Link>
+            
+            <button className="BtnP SecondClasBtn">
+              <span className="circleMovil" aria-hidden="true">
+              <span className="icon arrow"></span>
+              </span>
+              <span className="buttotext">DESTACADOS</span>
+            </button>
+
+          </div>
+        </div>
+        <video muted autoPlay loop >
+          <source src={ComputadoraVideo} type='video/mp4'/>
+        </video>
+        <div className="capa">
+          
+        </div>
+      </main>
+    </>
+  )
+}
+
+
+
+{/* <div className="contButtonsRs">
         
         <Link to='/Login'>
           <button className="BtnP SecondClasBtn">
@@ -38,10 +80,4 @@ export const PagehomeUp = () => {
           <span className="icon arrow"></span>
           </span>
           <span className="buttotext">DESTACADOS</span>
-        </button>
-
-      </div>
-    </div>
-    </>
-  )
-}
+        </button> */}
