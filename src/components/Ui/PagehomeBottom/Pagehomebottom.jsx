1bf2c0baa1ef0a944ebb0d903f1e9da46bdc6bed
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2023-04-12 20:22:06 -0500

    page bottom en proceso

--- src/components/Ui/PagehomeBottom/Pagehomebottom.jsx
+++ src/components/Ui/PagehomeBottom/Pagehomebottom.jsx
@@ -1,72 +1,42 @@
 import React from 'react'
 import './Pagehomebottom.css'
-import  logopc2do  from '../../../images/logopc2do.jpg'
-import cellProducto from '../../../images/productos/cellProducto.jpg'
-import pcProductoVenta from '../../../images/productos/pcProductoVenta.jpg'
+// import  logopc2do  from '../../../images/logopc2do.jpg'
+// import cellProducto from '../../../images/productos/cellProducto.jpg'
+// import pcProductoVenta from '../../../images/productos/pcProductoVenta.jpg'
 
 export const Pagehomebottom = () => {
   return (
-    <div className='contServicios'>
-        <div className="contTittleS">
-          <p>PRODUCTOS EN VENTA</p>
-        </div>
-        <div className="contCardsS">
-          <div className="card">
-            <div className="face front">
-              <img src={cellProducto} alt="..." />
-              <h3>Venta De Celular</h3>
-            </div>
-            <div className="face back">
-              <h3>venta de pc</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Rem commodi beatae quidem reprehenderit nostrum 
-              doloremque officiis sapiente
-              nam cumque dolorum, in nesciunt totam consectetur at quia 
-              iure quos!</p>
-              <div className="link">
-                <a href="#">Details</a>
-              </div>
-            </div>
+    <>
+      {/* services section */}
+      <section className='services' id='services'>
+        <h2 className='heading'>Our <span>Services</span></h2>
+
+        <div className="services-container">
+          <div className="services-box">
+            <i class='bx bxs-phone'></i>
+            <h3>Web Developer</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam, accusamus excepturi 
+              labore aut molestias accusantium laboriosam. Necessitatibus fugit officia veniam?</p>
+            <a href="#" className='btn'>Read More</a>
           </div>
 
-
-          <div className="card">
-            <div className="face front">
-              <img src={logopc2do} alt="..." />
-              <h3>Venta De Pc</h3>
-            </div>
-            <div className="face back">
-              <h3>venta de pc</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Rem commodi beatae quidem reprehenderit nostrum 
-              doloremque officiis sapiente
-              nam cumque dolorum, in nesciunt totam consectetur at quia 
-              iure quos!</p>
-              <div className="link">
-                <a href="#">Details</a>
-              </div>
-            </div>
+          <div className="services-box">
+          <i class='bx bx-share-alt' ></i>
+            <h3>Web Graphic</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam, accusamus excepturi 
+              labore aut molestias accusantium laboriosam. Necessitatibus fugit officia veniam?</p>
+            <a href="#" className='btn'>Read More</a>
           </div>
 
-
-          <div className="card">
-            <div className="face front">
-              <img src={pcProductoVenta} alt="..." />
-              <h3>Venta De Pc</h3>
-            </div>
-            <div className="face back">
-              <h3>venta de pc</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Rem commodi beatae quidem reprehenderit nostrum 
-              doloremque officiis sapiente
-              nam cumque dolorum, in nesciunt totam consectetur at quia 
-              iure quos!</p>
-              <div className="link">
-                <a href="#">Details</a>
-              </div>
-            </div>
+          <div className="services-box">
+            <i class='bx bx-upvote'></i>
+            <h3>Web Desing</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam, accusamus excepturi 
+              labore aut molestias accusantium laboriosam. Necessitatibus fugit officia veniam?</p>
+            <a href="#" className='btn'>Read More</a>
           </div>
         </div>
-    </div>
+      </section>
+    </>
   )
 }
