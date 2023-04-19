524be235f210d682c4e390894276f3d92db176e0
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2023-04-18 23:32:08 -0500

    avance en el responsive del navbar

--- src/components/Ui/PagehomeUp/PagehomeUp.jsx
+++ src/components/Ui/PagehomeUp/PagehomeUp.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
 import iconTecnologyModifi from '../../../images/icons/iconTecnologyModifi.png'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 // import ComputadoraVideo from '../../../images/video/ComputadoraVideo.mp4'
 import './PagehomeUp.css'
 
 export const PagehomeUp = () => {
+
+  /*=== toggle icon navbar ===*/
+  // let menuIcon = document.querySelector('#menu-icon')
+  // let navbar = document.querySelector('.navbar')
+  
+  // menuIcon.onclick = () => {
+  //   menuIcon.classList.toggle('bx-x')
+  //   navbar.classList.toggle('active')
+  // }
+
+  /* === ACTIVACION DE SCROLL PARA SECCIONES === */
+
+  let sections = document.querySelectorAll('section')
+  let navLinks = document.querySelectorAll('header nav a')
+
+  window.onscroll = () => {
+    sections.forEach(sec => {
+      let top = window.scrollY
+      let offset = sec.offsetTop - 150
+      let heigth = sec.offsetHeight
+      let id = sec.getAttribute('id')
+
+      if (top >= offset && top < offset + heigth) {
+        navLinks.forEach(links => {
+          links.classList.remove('active')
+          document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+        })
+      }
+    })
+  }
+
+  // let header = document.querySelector('header')
+  // header.classList.toggle('sticky', window.scrollY > 100)
+
   return (
     <>
       {/* header-design  */}
@@ -13,7 +47,7 @@ export const PagehomeUp = () => {
 
         <a href="#" className='logo'>Market technology</a>
 
-        <i class='bx bx-menu' id='menu-icon'></i>
+        <i className='bx bx-menu' id='menu-icon'></i>
 
         <nav className='navbar'>
           <a href='#home' className='Active'>Home</a>
