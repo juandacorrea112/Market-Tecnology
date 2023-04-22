e7df80a14ed18adc924ba5e41a0a3d661c45f79c
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2023-04-21 23:19:54 -0500

    eliminacion de linea innecesaria

--- src/components/Ui/PagehomeUp/PagehomeUp.jsx
+++ src/components/Ui/PagehomeUp/PagehomeUp.jsx
@@ -85,9 +85,6 @@ export const PagehomeUp = () => {
     })
   }
 
-  // let header = document.querySelector('header')
-  // header.classList.toggle('sticky', window.scrollY > 100)
-
   return (
     <>
       {/* header-design  */}
