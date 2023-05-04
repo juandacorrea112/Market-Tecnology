90c5f3d775f7a6df7d6bdfee5e47512ce9ce2a41
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2023-05-04 18:34:20 -0500

    barra de opciones de settings adelantada

--- src/components/Pages/PageSettings/PageSettings.jsx
+++ src/components/Pages/PageSettings/PageSettings.jsx
@@ -1,13 +1,38 @@
 import React from 'react'
+import girlProfile from '../../../images/perfiles/girlProfile.jpg'
 import './PageSettings.css'
 
 export const PageSettings = () => {
     return (
         <div className='contPhaterS'>
             <div className="contBoxprofile">
+
                 <div className="barInfoOptions">
-                    
+                    <div className="contImgP">
+                        <img src={girlProfile} alt="" />
+                    </div>
+                    <div className="contNameFull">
+                        <h3>Name User</h3>
+                    </div>
+                    <div className="contOptionsS">
+                        <div className="changeDates">
+                            <h3>Full name</h3>
+                        </div>
+                        <div className="changeDates">
+                            <h3>Phone</h3>
+                        </div>
+                        <div className="changeDates">
+                            <h3>Profile Picture</h3>
+                        </div>
+                        <div className="changeDates">
+                            <h3>Password</h3>
+                        </div>
+                        <div className="changeDates">
+                            <h3>Email</h3>
+                        </div>
+                    </div>
                 </div>
+
             </div>
         </div>
     )
