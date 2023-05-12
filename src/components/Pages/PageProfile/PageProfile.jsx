e726b06f06edfc34b546f866cd1e3ba55f17542e
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2023-05-11 21:19:27 -0500

    primera actualizacion del perfil publico de usuario

--- src/components/Pages/PageProfile/PageProfile.jsx
+++ src/components/Pages/PageProfile/PageProfile.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import $ from 'jquery';
+
 import girlProfile from '../../../images/perfiles/girlProfile.jpg'
 // import calularProducto from '../../../images/productos/calularProducto.jpg'
 import './PageProfile.css'
@@ -6,52 +8,27 @@ import './PageProfile.css'
 export const PageProfile = () => {
   return (
     <>
-    <div className="contCardprofile">
-      <div class="wrapper">
-        <div class="left">
-          <img src={girlProfile} alt={girlProfile} width="100"/>
-            <h4>Alex William</h4>
-            <p>Vendendor</p>
+      <header className='headerProfileUser'>
+        <div className="user">
+          <img src={girlProfile} alt={girlProfile} />
+          <h3 className='name'>Name user</h3>
+          <p className='post'>vendedor de productos</p>
         </div>
-        <div class="right">
-          <div class="info"> 
-            <h3>Information</h3>
-            <div class="info_data">
-              <div class="data">
-                <h4>Email</h4>
-                <p>alex@gmail.com</p>
-              </div>
-              <div class="data">
-                <h4>Phone</h4>
-                <p>0001-213-998761</p>
-              </div>
-            </div>
-          </div>
 
-          <div class="projects">
-            <h3>Description</h3>
-            <div class="projects_data">
-              <div class="data">
-                <h4>Recent</h4>
-                <p>Lorem ipsum dolor sit amet.</p>
-              </div>
-              <div class="data">
-                <h4>Most Viewed</h4>
-                <p>dolor sit amet.</p>
-              </div>
-            </div>
-          </div>
-
-          <div class="social_media">
-            <ul>
-              <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-              <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-              <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
+        <nav className='navbar'>
+          <ul>
+            <li><a href="#home">home</a></li>
+            <li><a href="#home">about</a></li>
+            <li><a href="#home">Portfolio</a></li>
+            <li><a href="#home">Contact</a></li>
+          </ul>
+        </nav>
+      </header>
     </>
   )
 }
+{/* <ul>
+  <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+  <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+  <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+</ul> */}
