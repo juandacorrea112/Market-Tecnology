a67a90c8e719e82a3c1261ae276fe4320f35e16e
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2023-05-12 22:03:46 -0500

    avances en perfil publico

--- src/components/Pages/PageProfile/PageProfile.jsx
+++ src/components/Pages/PageProfile/PageProfile.jsx
@@ -7,7 +7,7 @@ import './PageProfile.css'
 
 export const PageProfile = () => {
 
-  
+
   return (
     <div className='body-profile'>
       <header className='headerProfileUser'>
@@ -30,18 +30,32 @@ export const PageProfile = () => {
 
       {/* <div id='menu' className="fas fa-bars"></div> */}
 
+      <div className="infooptionsProfile">
+        <section id='home' className='homeprofileperson'>
+          <h3>Hi there !</h3>
+          <h1>I'M <span>shaikh anas</span></h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam suscipit doloremque, ad molestiae eius, debitis iste laboriosam commodi voluptatum laudantium minima repudiandae vitae architecto itaque voluptatibus saepe!</p>
+          <a href="#about"><button className='btnprofileperson'>About me <i className="fas fa-user"></i></button></a>
+        </section>
+        {/* home section ends */}
 
-      <section id='home' className='homeprofileperson'>
-        <h3>Hi there !</h3>
-        <h1>I'M <span>shaikh anas</span></h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam suscipit doloremque, ad molestiae eius, debitis iste laboriosam commodi voluptatum laudantium minima repudiandae vitae architecto itaque voluptatibus saepe!</p>
-        <a href="#about"><button className='btnprofileperson'>About me <i className="fas fa-user"></i></button></a>
-      </section>
-      {/* home section ends */}
 
+        {/* about sections starts */}
 
-      {/* about sections starts */}
-      
+        <section className="aboutProf" id='about'>
+          <h1 className="heading"><span>About</span> me </h1>
+
+          <div className="rowProf">
+            <div className="infoProf">
+              <h3><span> name : </span> shaikh anas</h3>
+              <h3><span> age : </span> vendedor </h3>
+              <h3><span> post : </span> shaikh anas</h3>
+              <h3><span> calification : </span> very god </h3>
+              <a href="#"><button className='btnprofileperson'>download CV <i className="fas fa-download"></i></button></a>
+            </div>
+          </div>
+        </section>
+      </div>
 
 
     </div>
