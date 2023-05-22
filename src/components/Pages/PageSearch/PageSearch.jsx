12a4cc9cbbd8b14714ebea04145f7a1b7b4580ec
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2023-05-22 15:44:32 -0500

    ajustes en cards establecidos

--- src/components/Pages/PageSearch/PageSearch.jsx
+++ src/components/Pages/PageSearch/PageSearch.jsx
@@ -40,83 +40,95 @@ export const PageSearch = () => {
 
                 </div>
                 <div className="BoxCards-P-and-S">
-                    <a href="#" class="card">
+                    <div href="#" class="card">
                         <img src={calularProducto} alt="..." className='card__img' />
-                            <span class="card__footer">
-                                <span>Awesome speedy card</span>
-                                <span>2 minutes!</span>
-                            </span>
-                            <span class="card__action">
-                                <svg viewBox="0 0 448 512" title="play">
-                                    <path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z" />
-                                </svg>
-                            </span>
-                    </a>
+                        <span class="card__footer">
+                            <span>Awesome speedy card</span>
+                            <span>2 minutes!</span>
+                        </span>
+                        <span class="card__action">
+                        <a href="#">
+                                <span class="material-symbols-outlined">
+                                    visibility
+                                </span>
+                            </a>
+                        </span>
+                    </div>
 
-                    <a href="#" class="card">
+                    <div href="#" class="card">
                         <img src={ctUppc} alt="..." className='card__img' />
-                            <span class="card__footer">
-                                <span>Awesome speedy card</span>
-                                <span>2 minutes!</span>
-                            </span>
-                            <span class="card__action">
-                                <svg viewBox="0 0 448 512" title="play">
-                                    <path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z" />
-                                </svg>
-                            </span>
-                    </a>
+                        <span class="card__footer">
+                            <span>Awesome speedy card</span>
+                            <span>2 minutes!</span>
+                        </span>
+                        <span class="card__action">
+                        <a href="#">
+                                <span class="material-symbols-outlined">
+                                    visibility
+                                </span>
+                            </a>
+                        </span>
+                    </div>
 
-                    <a href="#" class="card">
+                    <div href="#" class="card">
                         <img src={pcProductoVenta} alt="..." className='card__img' />
-                            <span class="card__footer">
-                                <span>Awesome speedy card</span>
-                                <span>2 minutes!</span>
-                            </span>
-                            <span class="card__action">
-                                <svg viewBox="0 0 448 512" title="play">
-                                    <path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z" />
-                                </svg>
-                            </span>
-                    </a>
+                        <span class="card__footer">
+                            <span>Awesome speedy card</span>
+                            <span>2 minutes!</span>
+                        </span>
+                        <span class="card__action">
+                            <a href="#">
+                                <span class="material-symbols-outlined">
+                                    visibility
+                                </span>
+                            </a>
+                        </span>
+                    </div>
 
-                    <a href="#" class="card">
+                    <div href="#" class="card">
                         <img src={cellProducto} alt="..." className='card__img' />
-                            <span class="card__footer">
-                                <span>Awesome speedy card</span>
-                                <span>2 minutes!</span>
-                            </span>
-                            <span class="card__action">
-                                <svg viewBox="0 0 448 512" title="play">
-                                    <path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z" />
-                                </svg>
-                            </span>
-                    </a>
+                        <span class="card__footer">
+                            <span>Awesome speedy card</span>
+                            <span>2 minutes!</span>
+                        </span>
+                        <span class="card__action">
+                            <a href="#">
+                                <span class="material-symbols-outlined">
+                                    visibility
+                                </span>
+                            </a>
+                        </span>
+                    </div>
 
-                    <a href="#" class="card">
+                    <div href="#" class="card">
                         <img src={dobolWindow} alt="..." className='card__img' />
-                            <span class="card__footer">
-                                <span>Awesome speedy card</span>
-                                <span>2 minutes!</span>
-                            </span>
-                            <span class="card__action">
-                                <svg viewBox="0 0 448 512" title="play">
-                                    <path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z" />
-                                </svg>
-                            </span>
-                    </a>
+                        <span class="card__footer">
+                            <span>Awesome speedy card</span>
+                            <span>2 minutes!</span>
+                        </span>
+                        <span class="card__action">
+                            <a href="#">
+                                <span class="material-symbols-outlined">
+                                    visibility
+                                </span>
+                            </a>
+                        </span>
+                    </div>
 
-                    <a href="#" class="card">
+                    <div class="card">
                         <img src={pcMack} alt="..." className='card__img' />
-                            <span class="card__footer">
-                                <span>Awesome speedy card</span>
-                                <span>2 minutes!</span>
-                            </span>
-                            <span class="card__action">
-                                <svg viewBox="0 0 448 512" title="play">
-                                    <path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z" />
-                                </svg>
-                            </span>
-                    </a>
+                        <span class="card__footer">
+                            <span>Awesome speedy card</span>
+                            <span>2 minutes!</span>
+                        </span>
+                        <span class="card__action">
+                            <a href="#">
+                                <span class="material-symbols-outlined">
+                                    visibility
+                                </span>
+                            </a>
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
