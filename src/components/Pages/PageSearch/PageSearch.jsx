1fd5759847dcfa3266b8f2f53b82c7722f8ddc77
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2023-05-30 21:49:24 -0500

    implementacion de la nueva card

--- src/components/Pages/PageSearch/PageSearch.jsx
+++ src/components/Pages/PageSearch/PageSearch.jsx
@@ -69,7 +69,7 @@ export const PageSearch = () => {
                 </div>
                 <div className="BoxCards-P-and-S">
 
-                    {(loadChatacter == 0) &&
+                    {/* {(loadChatacter == 0) &&
                         character.map((dat) => (
                             <div key={dat.id} href="#" class="card">
                                 <img src={dat.image} alt="..." className='card__img' />
@@ -120,7 +120,32 @@ export const PageSearch = () => {
                                 </a>
                             </span>
                         </div>
-                    ))}
+                    ))} */}
+
+
+
+                    {/* new card  */}
+
+                    <article class="card">
+                        <img
+                            class="card__background"
+                            src="https://i.imgur.com/QYWAcXk.jpeg"
+                            alt="Photo of Cartagena's cathedral at the background and some colonial style houses"
+                            width="1920"
+                            height="2193"
+                        />
+                        <div class="card__content | flow">
+                            <div class="card__content--container | flow">
+                                <h2 class="card__title">Colombia</h2>
+                                <p class="card__description">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum in
+                                    labore laudantium deserunt fugiat numquam.
+                                </p>
+                            </div>
+                            <button class="card__button">Read more</button>
+                        </div>
+                    </article>
+
                 </div>
             </div>
         </div>
