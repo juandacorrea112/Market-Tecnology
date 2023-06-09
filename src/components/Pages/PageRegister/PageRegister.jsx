92a0a80790a07912874b692ee788fbc3f065ef6e
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2023-06-08 21:28:04 -0500

    animacion de label del formulario de registro cargada y establecido

--- src/components/Pages/PageRegister/PageRegister.jsx
+++ src/components/Pages/PageRegister/PageRegister.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './PageRegister.css'
 
 export const PageRegister = () => {
+    
+    /*==================== cargar animacion de label ====================*/
+    useEffect(() => {
+        let label = document.querySelectorAll('#labelUp').forEach(label => {
+            label.innerHTML = label.innerText.split('').map((letters, i) => `<span style="transition-delay: ${
+                i * 50
+            }ms">${letters}</span>`).join('')
+        })
+    },[])
+
+
   return (
     <div className='ContRegister'>
         <div className="lentRegister"></div>
@@ -12,19 +23,19 @@ export const PageRegister = () => {
             <h1>Register</h1>
             <div className='inputBox'>
                 <input className='inputinterB' type="text" required/>
-                <label>Username</label>
+                <label id='labelUp'>Username</label>
             </div>
             <div className='inputBox'>
                 <input className='inputinterB' type="email" required/>
-                <label>Email</label>
+                <label id='labelUp'>Email</label>
             </div>
             <div className="inputBox">
                 <input className='inputinterB' type="password" required/>
-                <label>Create Password</label>
+                <label id='labelUp'>Create Password</label>
             </div>
             <div className="inputBox">
                 <input id='inputinterB' className='inputinterB' type="password" required/>
-                <label>Confirm Password</label>
+                <label id='labelUp'>Confirm Password</label>
             </div>
             <div className="inputBox">
 
@@ -32,61 +43,6 @@ export const PageRegister = () => {
             </div>
             <p>Alreadey have account ? <a href="/Login">Login</a></p>
         </form>
-
-
-
-
-
-        {/* <form className='FormRegister'>
-            <div className="contTittleRegister">
-                <h2>REGISTRO</h2>
-            </div>
-            
-            <div className="contDivsInputs">
-                <div className="ContsNames">
-                    <div className="inputBoxR">
-                        <input className='EmailRe' type="text" required />
-                        <span className='EmailR'>Nombres</span>
-                    </div>
-
-                    <div className="inputBoxR">
-                        <input className='EmailRe' type="text" required />
-                        <span className='EmailR'>apellidos</span>
-                    </div>
-
-                    <div className="inputBoxR">
-                        <input className='EmailRe' type="email" required />
-                        <span className='EmailR'>Correo</span>
-                    </div>
-
-                </div>
-
-                <div className="ContPasswords">
-
-                    <div className="inputBoxR">
-                        <input className='EmailRe' type="password" required />
-                        <span className='EmailR'>Contraseña</span>
-                    </div>
-
-                    <div className="inputBoxR">
-                        <input className='EmailRe' type="password" required />
-                        <span className='EmailR'>Confirmar Contraseña</span>
-                    </div>
-
-                    <div className="inputBoxR">
-                        <input className='EmailRe' type="number" required />
-                        <span className='EmailR'>Celular</span>
-                    </div>
-
-                </div>
-            </div>
-
-            <div className="contButtonR">
-                <button type='sumbit' className='BtnRegister'>CONFIRMAR</button>
-            </div>
-
-        </form> */}
-
     </div>
   )
 }
